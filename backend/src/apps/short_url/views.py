52fc189379827b6d6c29593737d77d5cbf841864
Dawid <rygula.dawid@gmail.com>
2024-03-18 18:18:37 +0100

    [tq-5] ShorURLViewSet should delegate ShortURL operations to celery tasks

--- backend/src/apps/short_url/views.py
+++ backend/src/apps/short_url/views.py
@@ -2,9 +2,29 @@ from rest_framework import viewsets
 
 from src.apps.short_url.models import ShortURL
 from src.apps.short_url.serializers import ShortURLSerializer
+from src.apps.short_url.tasks import (
+    create_short_url,
+    delete_short_url,
+    update_short_url,
+)
 
 
 class ShorURLViewSet(viewsets.ModelViewSet):
     queryset = ShortURL.objects.all()
     serializer_class = ShortURLSerializer
     lookup_field = "url_squid"
+
+    def perform_create(self, serializer):
+        create_short_url.delay(
+            name=serializer.validated_data["name"], url=serializer.validated_data["url"]
+        )
+
+    def perform_update(self, serializer):
+        update_short_url.delay(
+            url_squid=serializer.instance.url_squid,
+            name=serializer.validated_data["name"],
+            url=serializer.validated_data["url"],
+        )
+
+    def perform_destroy(self, instance):
+        delete_short_url.delay(url_squid=instance.url_squid)
