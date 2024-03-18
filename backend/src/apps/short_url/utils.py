cd4a4b739d11a5c1ef5db60c78b626dc8f5606ea
Dawid <rygula.dawid@gmail.com>
2024-03-18 15:16:10 +0100

    [tq-4] get_url_squid should encode ShortURL id

--- backend/src/apps/short_url/utils.py
+++ backend/src/apps/short_url/utils.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from sqids import Sqids
 
 
-def get_url_squid():
-    return Sqids(min_length=settings.SHORT_URL_SQUID_LENGTH)
+def get_url_squid(id: int):
+    return Sqids(min_length=settings.SHORT_URL_SQUID_LENGTH).encode([id])
