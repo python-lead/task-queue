803bcdc20e1653e3d3c41d51ac5a282863fa0305
Dawid <rygula.dawid@gmail.com>
2024-03-18 12:32:29 +0100

    [tq-2] Configure Django postgresql database

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "src.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "CONN_MAX_AGE": 100,
     }
 }
 
