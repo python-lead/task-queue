c254536867608991883fe2f148ea0af162505800
Dawid <rygula.dawid@gmail.com>
2024-03-18 15:12:04 +0100

    [tq-4] Ensure unique squid on ShortURL creation

--- backend/src/apps/short_url/models.py
+++ backend/src/apps/short_url/models.py
@@ -5,10 +5,11 @@ from src.apps.short_url.utils import get_url_squid
 
 
 class ShortURL(models.Model):
-    url = models.URLField(max_length=256, unique=True)
-    url_squid = models.CharField(max_length=16, default=get_url_squid, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=32, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
+    url = models.URLField(max_length=256, unique=True)
+    url_squid = models.CharField(max_length=16, unique=True)
 
     def __str__(self):
         return f"ShortURL: {self.url_squid}"
@@ -16,3 +17,17 @@ class ShortURL(models.Model):
     @property
     def short_url(self):
         return f"{settings.BACKEND_HOST}/{self.url_squid}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.url_squid:
+            self.url_squid = self._generate_unique_squid()
+            self.save()
+
+    def _generate_unique_squid(self):
+        squid = get_url_squid(self.id)
+        while ShortURL.objects.filter(url_squid=squid).exists():
+            squid = get_url_squid(self.id)
+
+        return squid
