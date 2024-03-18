1327636905188284607ff8b9571c139231c43709
Dawid <rygula.dawid@gmail.com>
2024-03-18 13:08:44 +0100

    [tq-3] Configure API routing urls

---
+++ backend/src/api_urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("api-auth/", include("rest_framework.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+]
